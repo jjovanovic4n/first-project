f0e66d868f3bb79cc88c4cf7c480953a6f30172e
Jelena <jelenajovanovic@yahoo.com>
2020-08-10 00:00:08 +0200

    Redesign website, second version in progress. Completed header and secondary navbar.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
+
+    window.addEventListener("scroll", this.scrollNavFunc);
   }
 
+  scrollNavFunc () {
+   let  classID = document.getElementById("navbar-secondary");
+        let y = window.scrollY;
+        if (y >= 750) {
+          classID.className = "bottomMenu show"
+        } else {
+          classID.className = "bottomMenu hide"
+        }
+      }
+  
 }
